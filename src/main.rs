867135eabd13fd8e7e693dec987d1d6b5d5b95de
Léderer András <andrew@file.glass>
2021-11-15 00:56:00 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+#![feature(async_stream)]
+
+mod test;
+
+use std::borrow::Borrow;
+use tokio::fs::File;
+use tokio;
+use reqwest;
+use reqwest::{Body, Error, StatusCode};
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::multipart;
+use serde::Deserialize;
+use tokio_util::codec::{BytesCodec, FramedRead};
+use crate::test::test::test;
+
+#[tokio::main]
+async fn main() {
+
+}
+
+
+pub struct Arachnid {
+    key: String,
+    url: String
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ArachnidResp {
+    pub classification: String,
+    pub distance: i32,
+}
+#[derive(Debug, Deserialize)]
+pub struct ArachnidError {
+error: String
+}
+
+impl Default for ArachnidError {
+    fn default() -> Self {
+        ArachnidError {error: "".to_string()}
+    }
+}
+
+#[derive(Debug)]
+pub struct ArachnidResult {
+    raw_resp: Vec<ArachnidResp>,
+    err: ArachnidError,
+    status_code: StatusCode,
+    errored: bool,
+    safe: bool
+}
+
+impl Arachnid {
+    pub fn new(key: String, url: String) -> Arachnid {
+        Arachnid {key, url}
+    }
+
+    pub async fn check_file(self: &Arachnid, file: File, file_name: String, mime: String) -> Result<ArachnidResult, Error>  {
+        let stream = FramedRead::new(file, BytesCodec::new());
+        let data = multipart::Part::stream(Body::wrap_stream(stream)).file_name(file_name).mime_str(&mime)?;
+        let form = multipart::Form::new().part("image", data);
+        let client = reqwest::Client::new();
+        let mut hd = HeaderMap::new();
+        hd.insert("Authorization", HeaderValue::from_str(self.key.as_str()).unwrap());
+        let res = client.post(&self.url).headers(hd).multipart(form).send().await?;
+        let stat = res.status();
+        if stat == 200 {
+            let resp: Vec<ArachnidResp> = res.json().await?;
+            let len = (&resp).len();
+            Ok(ArachnidResult { raw_resp: resp, err: ArachnidError::default(), status_code: stat, safe: len == 0, errored: false})
+        } else {
+            let resp: ArachnidError = res.json().await.unwrap();
+            Ok(ArachnidResult { raw_resp: vec![], err: resp, status_code: stat, safe: false, errored: true })
+        }
+    }
+}
+
+
---
+++ src/test.rs
@@ -0,0 +1,14 @@
+#![feature(async_stream)]
+#[cfg(test)]
+pub mod test {
+    use tokio::fs::File;
+    use crate::Arachnid;
+    use tokio;
+    #[tokio::test]
+    pub async fn test() {
+        let client = Arachnid::new("KEY".to_string(), "URL".to_string());
+        let f = File::open("test.jpg").await.unwrap();
+        let res = client.check_file(f, "test.jpg".to_string(), "image/jpeg".to_string()).await.unwrap();
+        dbg!(res);
+    }
+}
